7603c4fc759bd41ba03cb07af4b36ce25174faee
Allcodingworld <allcodingworld@gmail.com>
2017-04-14 10:58:20 +0530

    update index.php with navigation code
    navigatin code

--- index.php
+++ index.php
@@ -1,1 +1,62 @@
 
+<!DOCTYPE html>
+<html>
+<head>
+  </head>
+ <body>
+  <div class="container">
+  
+  <!----navbar----------------->
+  <nav class="navbar navbar-default" role="navigation">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Software Solutions</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li><a href="#"><span class="glyphicon glyphicon glyphicon-home"></span> Home</a></li>
+        
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown"  data-close-others="false">
+          <span class="glyphicon glyphicon glyphicon-user"></span> 
+          About Us
+          <span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#"><span class="glyphicon glyphicon glyphicon-chevron-right"></span> Vission</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon glyphicon-chevron-right"></span> Mission</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon glyphicon-chevron-right"></span> Portfolio</a></li>
+          </ul>
+        </li>
+        <li><a href="#"> <span class="glyphicon glyphicon glyphicon-pencil"></span> Testimonial</a></li>
+      </ul>
+     
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon glyphicon-paperclip"></span> Broucher</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="false">
+          <span class="glyphicon glyphicon glyphicon-earphone"></span> 
+          Contact 
+          <span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#"><span class="glyphicon glyphicon glyphicon-screenshot"></span> Sitemap</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon glyphicon-user"></span> Career</a></li>
+            
+          </ul>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+</div>
+<!----/////----navbar----------------->
+  </body>
+</html>
